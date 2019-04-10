fc0164e9d474aa9554c29fa1ef1f1b7e376efeda
Leo van Haaren <leovhaaren@gmail.com>
2019-04-10 10:35:59 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# whatsapp-clone
+
+Partially based upon 
+https://github.com/Urigo/WhatsApp-Clone-Tutorial
+
+## Tech used
+- React
+- Typescript
+- GraphQL
+- Express
---
+++ client/src/index.tsx
@@ -0,0 +1,34 @@
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo-hooks';
+
+import './index.css';
+import App from './App';
+import apolloClient from './apollo-client';
+
+import * as serviceWorker from './serviceWorker';
+
+// TODO: cleanup this with a token generator
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2c6157' },
+    secondary: { main: '#6fd056' },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </MuiThemeProvider>
+  , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
